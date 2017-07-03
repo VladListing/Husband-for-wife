aa1d1d0974ab5d08f06881d9f4d62f4ac2001207
VladListing <chvch@mail.ru>
2017-07-03 23:54:24 +0700

    creation  class 'TaxiDriver'

---
+++ Husband for wife/TaxiDriver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Husband_for_wife
+{
+   
+    class TaxiDriver
+    {
+        private int _money = 0;
+        public int GetMoney()
+        {
+            return _money + 10;
+        }
+
+    }
+}
