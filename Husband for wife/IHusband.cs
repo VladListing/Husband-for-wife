890b8eca29d36fefd69c9ce77913b13a0d13ddef
VladListing <chvch@mail.ru>
2017-07-03 23:47:56 +0700

    creation 'IHusband' and class 'Programmer'

---
+++ Husband for wife/IHusband.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Husband_for_wife
+{
+    interface IHusband
+    {
+        //метод зарабатывания денег
+         int GetMoney();
+    }
+}
